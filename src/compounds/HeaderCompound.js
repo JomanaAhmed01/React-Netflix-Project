b83206c4a31ce58f166109b86748d07466f31c66
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2020-09-10 01:05:33 +0200

    Add HeaderWrapper to HeaderCompound.

--- src/compounds/HeaderCompound.js
+++ src/compounds/HeaderCompound.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import HeaderContainer from '../components/HeaderContainer';
+import HeaderWrapper from '../components/Header/HeaderWrapper'
 
 function HeaderCompund() {
     return (
+        <>
         <HeaderContainer />
+        <HeaderWrapper />
+        </>
     )
 }
 
