fef963ef82f0c9df361b7c93b87f7e2d9b719c02
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2020-09-10 01:12:00 +0200

    Add styles for HeaderWrapper

--- src/components/Header/HeaderWrapper.js
+++ src/components/Header/HeaderWrapper.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function HeaderWrapper() {
+function HeaderWrapper({children}) {
     return (
-        <></>
+    <div className="header-wrapper">{children}</div>
     )
 }
 
