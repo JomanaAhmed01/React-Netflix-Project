06013830d6a7d1a4bc846b25ee8934ebc966ad40
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2020-09-10 00:46:17 +0200

    Build HeaderCompound Component.

---
+++ src/compounds/HeaderCompound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HeaderContainer from '../components/HeaderContainer';
+
+function HeaderCompund() {
+    return (
+        <HeaderContainer />
+    )
+}
+
+export default HeaderCompund;
