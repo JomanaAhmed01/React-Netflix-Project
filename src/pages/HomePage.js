4cd96114ff3e95600ec12039d3189fdb51a9d67c
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2020-09-10 00:47:33 +0200

    Add HeaderCompound to HomePage.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,10 @@
 import React from "react";
 import HeaderContainer from "../components/HeaderContainer";
+import HeaderCompund from "../compounds/HeaderCompound";
 
 function HomePage() {
     return (
-        <HeaderContainer />
+        <HeaderCompund />
     )
 }
 
