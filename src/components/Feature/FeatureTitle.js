f68836b0380ce72766d048dead360f2579e2174f
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2020-09-11 02:49:44 +0200

    Build FeaturTitle Component.

---
+++ src/components/Feature/FeatureTitle.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function FeatureTitle() {
+    return (
+        <h1 className="feature-title">Unlimited movies, TV <br /> shows, and more.</h1>
+    )
+}
+
+export default FeatureTitle;
--- src/compounds/HeaderCompound.js
+++ src/compounds/HeaderCompound.js
@@ -4,6 +4,7 @@ import Logo from '../components/Header/Logo.js';
 import NavbarContainer from '../components/Header/NavbarContainer';
 import SigninBtn from '../components/Header/SigninBtn';
 import FeatureWrapper from '../components/Feature/FeatureWrapper';
+import FeatureTitle from '../components/Feature/FeatureTitle';
 
 function HeaderCompund() {
     return (
@@ -14,7 +15,7 @@ function HeaderCompund() {
             </NavbarContainer>
 
             <FeatureWrapper>
-                
+                <FeatureTitle />
             </FeatureWrapper>
         </HeaderContainer>
     )
