4c13e0501f6798432db164c0383760b6e4671ef4
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2020-09-17 16:28:11 +0200

    Add OptFormWrapper to OptFormCompound.

---
+++ src/compounds/OptFormCompound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import OptFormWrapper from '../components/OptForm/OptFormWrapper'
+
+function OptFormCompound() {
+    return (
+        <OptFormWrapper />
+    )
+}
+
+export default OptFormCompound;
