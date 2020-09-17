88c879fc9f6ed3250a785e927c3ec449394b2baf
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2020-09-17 16:39:32 +0200

    Build OptFormEmail Component.

---
+++ src/components/OptForm/OptFormEmail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function OptFormEmail() {
+    return (
+        <>
+          <input type="text" placeholder="Email address"></input>
+        </>
+    )
+}
+
+export default OptFormEmail;
