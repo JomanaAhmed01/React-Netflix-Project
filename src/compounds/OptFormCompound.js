6e369132295fd06e02905898cf4d914f2e939a36
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2020-09-17 16:40:49 +0200

    Add OptFormEmail to OptFormCompound.

--- src/compounds/OptFormCompound.js
+++ src/compounds/OptFormCompound.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import OptFormWrapper from '../components/OptForm/OptFormWrapper'
 import OptFormText from '../components/OptForm/OptFormText'
+import OptFormEmail from '../components/OptForm/OptFormEmail'
 
 function OptFormCompound() {
     return (
         <OptFormWrapper>
            <OptFormText />
+           <OptFormEmail />
         </OptFormWrapper>
     )
 }
