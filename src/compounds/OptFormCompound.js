1bda1c0bbadc7879a9446e10c975731469268994
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2020-09-17 16:45:40 +0200

    Add OptFormButton to OptFormCompound.

--- src/compounds/OptFormCompound.js
+++ src/compounds/OptFormCompound.js
@@ -2,12 +2,14 @@ import React from 'react';
 import OptFormWrapper from '../components/OptForm/OptFormWrapper'
 import OptFormText from '../components/OptForm/OptFormText'
 import OptFormEmail from '../components/OptForm/OptFormEmail'
+import OptFormBtn from '../components/OptForm/OptFormBtn';
 
 function OptFormCompound() {
     return (
         <OptFormWrapper>
            <OptFormText />
            <OptFormEmail />
+           <OptFormBtn />
         </OptFormWrapper>
     )
 }
