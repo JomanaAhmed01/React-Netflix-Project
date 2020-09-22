326f656b13f937f6e856e9bb0bfb25e513bdb38f
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2020-09-22 22:28:12 +0200

    Add JumboCompound to HomePage.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HeaderCompound from "../compounds/HeaderCompound";
+import JumboCompound from "../compounds/JumboCompound";
 
 function HomePage() {
     return (
         <>
         <HeaderCompound />
+        <JumboCompound />
         </>
     )
 }
