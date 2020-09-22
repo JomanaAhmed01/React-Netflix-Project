aef7560e03104429c76d3ba7e6a0177de0c73311
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2020-09-22 22:43:14 +0200

    Add JumpoTextWrapper to JumboCompound.

--- src/compounds/JumboCompound.js
+++ src/compounds/JumboCompound.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import JumboItem from '../components/Jumbo/JumboItem';
+import JumboTextWrapper from '../components/Jumbo/JumboTextWrapper';
 import JumboWrapper from '../components/Jumbo/JumboWrapper';
 
 function JumboCompound() {
     return (
         <JumboWrapper>
             <JumboItem />
+            <JumboTextWrapper />
         </JumboWrapper>
     )
 }
