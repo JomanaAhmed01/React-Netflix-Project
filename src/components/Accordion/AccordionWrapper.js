462119e0375ef572eb0bf3786cf0f3964b63d276
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2020-09-29 16:58:40 +0200

    Build AccordionWrapper Component. - Add AccordionWrapper to AccordionCompound.

---
+++ src/components/Accordion/AccordionWrapper.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AccordionWrapper() {
+    return (
+        <></>
+    )
+}
+
+export default AccordionWrapper;
--- src/compounds/AccordionCompound.js
+++ src/compounds/AccordionCompound.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import AccordionWrapper from '../components/Accordion/AccordionWrapper';
 
 function AccordionCompound() {
     return (
-        <></>
+        <AccordionWrapper />
     )
 }
 
