0f870643ab21f46ebc573784b100f1ea89c20b7b
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2020-09-29 16:55:31 +0200

    Build AccordionCompound Component. - Add AccordionCompound to HomePage.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,7 @@ import React from "react";
 import HeaderCompound from "../compounds/HeaderCompound";
 import JumboCompound from "../compounds/JumboCompound";
 import Seperator from "../components/Jumbo/Seperator"
+import AccordionCompound from "../compounds/AccordionCompound";
 
 function HomePage() {
     return (
@@ -9,6 +10,7 @@ function HomePage() {
         <HeaderCompound />
         <Seperator />
         <JumboCompound />
+        <AccordionCompound />
         </>
     )
 }
