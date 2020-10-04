2b0a53328baa4773130ea1ca183e28c32278d63d
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2020-10-04 22:14:10 +0200

    Build FooterWrapper Component. - - Add FooterWrapper to FooterCompound.

--- src/compounds/FooterCompound.js
+++ src/compounds/FooterCompound.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import FooterWrapper from "../components/Footer/FooterWrapper";
 import Seperator from '../components/Jumbo/Seperator';
 
 function FooterCompound() {
     return (
+      <>
         <Seperator />
+        <FooterWrapper>
+
+        </FooterWrapper>
+      </>
     )
 }
 
