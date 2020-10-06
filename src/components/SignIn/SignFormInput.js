a650056618e7bc5be91ba222dc9f544b8906805d
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2020-10-06 23:17:51 +0200

    Adding Password input

--- src/components/SignIn/SignFormInput.js
+++ src/components/SignIn/SignFormInput.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 function SignFormInput({children, ...restProps}) {
     return (
-    <div {...restProps}>{children}<input className="signform-input" type="text" placeholder="Email or phone number"></input></div>
+      <>
+        <div {...restProps}>{children}<input className="signform-input" type="text" placeholder="Email or phone number"></input></div>
+
+        <div {...restProps}>{children}<input className="signform-password" type="password" placeholder="Password"></input></div>
+      </>
     )
 }
 
