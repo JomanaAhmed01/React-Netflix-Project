ce43a988ed777ca5b5c975255bf2143e5417e0e9
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2020-10-10 21:29:26 +0200

    Copy SigninPage, Rename it to SignupPage - Editing the SignFormBtn

--- src/components/SignIn/SignFormBtn.js
+++ src/components/SignIn/SignFormBtn.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function SignFormBtn({children, ...restProps}) {
     return (
-    <div {...restProps}>{children}<a className="signform-btn" href="#">Sign In</a></div>
+    <div {...restProps}>{children}<button className="signform-btn">Sign In</button></div>
     )
 }
 
--- src/components/SignIn/SignFormInput.js
+++ src/components/SignIn/SignFormInput.js
@@ -3,9 +3,10 @@ import React from 'react';
 function SignFormInput({children, ...restProps}) {
     return (
       <>
-        <div {...restProps}>{children}<input className="signform-input" type="text" placeholder="Email or phone number"></input></div>
-
-        <div {...restProps}>{children}<input className="signform-password" type="password" placeholder="Password"></input></div>
+        <div className="signform-input" {...restProps}>
+          {children}
+          <input {...restProps}></input>
+        </div>
       </>
     )
 }
