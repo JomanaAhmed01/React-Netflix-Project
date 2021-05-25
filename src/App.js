38e34dbddec19f8cfde288811168b6cc021b6a08
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-05-25 21:02:50 +0200

    Creating the Header Section ( Without Responsive design yet )

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import React from 'react';
 import HomePage from "../src/pages/HomePage"
 import SignInPage from '../src/pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
+import Browse from './pages/Browse'
 
 function App() {
   return (
-     <Router>
-       <Switch>
-         <Route exact path="/">
-            <HomePage />
-         </Route>
-         <Route path="/signin">
-            <SignInPage />
-         </Route>
-         <Route path="/signup">
-            <SignUpPage />
-         </Route>
-       </Switch>
-     </Router>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/signin">
+          <SignInPage />
+        </Route>
+        <Route path="/signup">
+          <SignUpPage />
+        </Route>
+        <Route path="/browse">
+          <Browse />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
---
+++ src/pages/Browse.js
@@ -0,0 +1,32 @@
+import React from "react"
+import Logo from "../components/Header/Logo.js"
+
+function Browse() {
+  return (
+    <div className="browse-wrapper">
+      <div className="browse-navbar">
+        <Logo className="browse-logo" />
+        <div className="browse-navbar-text">
+          <a href="#" className="browse-films">
+            Films
+          </a>
+          <a href="#" className="browse-series">
+            Series
+          </a>
+        </div>
+      </div>
+
+      <div className="browse-header">
+        <p className="browse-title">Watch The Irish Man</p>
+        <p className="browse-subtitle">
+          Hit man Frank Sheeran looks back at the secrets he kept as a loyal
+          member of the Bufalino crime family in this acclaimed film from Martin
+          Scorsese.
+        </p>
+        <button className="browse-header-play-button">Play</button>
+      </div>
+    </div>
+  )
+}
+
+export default Browse
