f356c2ce5a1265756c7aad3e520dec1ace64907a
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-05-26 22:15:07 +0200

    Creating the 2nd part in the Browse Page

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -5,26 +5,41 @@ import Logo from "../components/Header/Logo.js"
 function Browse() {
   return (
     <div className="browse-wrapper">
-      <div className="browse-navbar">
-        <Logo className="browse-logo" />
-        <div className="browse-navbar-text">
-          <a href="#" className="browse-films">
-            Films
-          </a>
-          <a href="#" className="browse-series">
-            Series
-          </a>
+      <div className="browse-first-part">
+        <div className="browse-navbar">
+          <Logo className="browse-logo" />
+          <div className="browse-navbar-text">
+            <a href="#" className="browse-films">
+              Films
+            </a>
+            <a href="#" className="browse-series">
+              Series
+            </a>
+          </div>
+        </div>
+
+        <div className="browse-header">
+          <p className="browse-title">Watch The Irish Man</p>
+          <p className="browse-subtitle">
+            Hit man Frank Sheeran looks back at the secrets he kept as a loyal
+            member of the Bufalino crime family in this acclaimed film from
+            Martin Scorsese.
+          </p>
+          <button className="browse-header-play-button">Play</button>
         </div>
       </div>
 
-      <div className="browse-header">
-        <p className="browse-title">Watch The Irish Man</p>
-        <p className="browse-subtitle">
-          Hit man Frank Sheeran looks back at the secrets he kept as a loyal
-          member of the Bufalino crime family in this acclaimed film from Martin
-          Scorsese.
-        </p>
-        <button className="browse-header-play-button">Play</button>
+      <div className="browse-movies">
+        <div className="browse-drama">
+          <p className="browse-drama-title">Drama</p>
+          <div className="browse-drama-movies">
+            <img
+              className="browse-drama-1st"
+              src="/images/films/drama/fight-club/small.jpg"
+              alt="Fight Club"
+            ></img>
+          </div>
+        </div>
       </div>
     </div>
   )
