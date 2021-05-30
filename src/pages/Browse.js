7751664ea98e0e4e41fa67a4c03ad91c4b90e647
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-05-30 17:28:33 +0200

    Creating the Information & Functionality of all the Drama Movies

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -8,6 +8,10 @@ import FooterWrapper from "../components/Footer/FooterWrapper"
 
 function Browse() {
   const [showFightClubInfo, setShowFightClubInfo] = useState(false)
+  const [showThePrestigeInfo, setShowThePrestigeInfo] = useState(false)
+  const [showTheSocialNetworkInfo, setShowTheSocialNetworkInfo] = useState(false)
+  const [showTheRevenantInfo, setShowTheRevenantInfo] = useState(false)
+  const [showKingsSpeechInfo, setShowKingsSpeechInfo] = useState(false)
 
   return (
     <div className="browse-wrapper">
@@ -36,6 +40,7 @@ function Browse() {
       </div>
 
       <div className="browse-movies">
+        {/* ************** Start Of The Drama Movies ******************* */}
         <div className="browse-drama">
           <p className="browse-drama-title">Drama</p>
           <div className="browse-drama-movies">
@@ -49,23 +54,29 @@ function Browse() {
               className="browse-drama-pic"
               src="/images/films/drama/the-prestige/small.jpg"
               alt="The Prestige"
+              onClick={() => setShowThePrestigeInfo(true)}
             ></img>
             <img
               className="browse-drama-pic"
               src="/images/films/drama/the-social-network/small.jpg"
               alt="The Social Network"
+              onClick={() => setShowTheSocialNetworkInfo(true)}
             ></img>
             <img
               className="browse-drama-pic"
               src="/images/films/drama/the-revenant/small.jpg"
               alt="The Revenant"
+              onClick={() => setShowTheRevenantInfo(true)}
             ></img>
             <img
               className="browse-drama-pic"
               src="/images/films/drama/kings-speech/small.jpg"
               alt="Kings Speech"
+              onClick={() => setShowKingsSpeechInfo(true)}
             ></img>
           </div>
+
+          {/* ***************** Fight Club Info ******************* */}
 
           {showFightClubInfo ? (
             <div className="browse-drama-fight-club">
@@ -90,7 +101,122 @@ function Browse() {
               </div>
             </div>
           ) : null}
+
+          {/* ***************** The Prestige Info ******************* */}
+
+          {showThePrestigeInfo ? (
+            <div className="browse-drama-the-prestige">
+              <div className="browse-drama-the-prestige-desc">
+                <div className="browse-drama-the-prestige-title-and-icon-wrapper">
+                  <p className="browse-drama-the-prestige-title">
+                    The Prestige
+                  </p>
+                  <div className="browse-drama-the-prestige-close-icon-wrapper">
+                    <img
+                      className="browse-drama-the-prestige-close-icon"
+                      src="/images/icons/close.png"
+                      onClick={() => setShowThePrestigeInfo(false)}
+                    ></img>
+                  </div>
+                </div>
+                <p className="browse-drama-the-prestige-subtitle">
+                  Two friends and fellow magicians become bitter enemies after a
+                  sudden tragedy. As they devote themselves to this rivalry,
+                  they make sacrifices that bring them fame but with terrible
+                  consequences.
+                </p>
+                <button className="browse-header-play-button">Play</button>
+              </div>
+            </div>
+          ) : null}
+
+          {/* ***************** The Social Network Info ******************* */}
+
+          {showTheSocialNetworkInfo ? (
+            <div className="browse-drama-the-social-network">
+              <div className="browse-drama-the-social-network-desc">
+                <div className="browse-drama-the-social-network-title-and-icon-wrapper">
+                  <p className="browse-drama-the-social-network-title">
+                    The Social Network
+                  </p>
+                  <div className="browse-drama-the-social-network-close-icon-wrapper">
+                    <img
+                      className="browse-drama-the-social-network-close-icon"
+                      src="/images/icons/close.png"
+                      onClick={() => setShowTheSocialNetworkInfo(false)}
+                    ></img>
+                  </div>
+                </div>
+                <p className="browse-drama-the-social-network-subtitle">
+                  Mark Zuckerberg creates a social networking site, Facebook,
+                  with the help of his friend Eduardo Saverin. But soon, a
+                  string of lies tears their relationship apart even as Facebook
+                  connects people.
+                </p>
+                <button className="browse-header-play-button">Play</button>
+              </div>
+            </div>
+          ) : null}
+
+          {/* ***************** The Revenant Info ******************* */}
+
+          {showTheRevenantInfo ? (
+            <div className="browse-drama-the-revenant">
+              <div className="browse-drama-the-revenant-desc">
+                <div className="browse-drama-the-revenant-title-and-icon-wrapper">
+                  <p className="browse-drama-the-revenant-title">
+                    The Revenant
+                  </p>
+                  <div className="browse-drama-the-revenant-close-icon-wrapper">
+                    <img
+                      className="browse-drama-the-revenant-close-icon"
+                      src="/images/icons/close.png"
+                      onClick={() => setShowTheRevenantInfo(false)}
+                    ></img>
+                  </div>
+                </div>
+                <p className="browse-drama-the-revenant-subtitle">
+                  Hugh Glass, a legendary frontiersman, is severely injured in a
+                  bear attack and is abandoned by his hunting crew. He uses his
+                  skills to survive and take revenge on his companion, who
+                  betrayed him.
+                </p>
+                <button className="browse-header-play-button">Play</button>
+              </div>
+            </div>
+          ) : null}
+
+          {/* ***************** Kings Speech Info ******************* */}
+
+          {showKingsSpeechInfo ? (
+            <div className="browse-drama-kings-speech">
+              <div className="browse-drama-kings-speech-desc">
+                <div className="browse-drama-kings-speech-title-and-icon-wrapper">
+                  <p className="browse-drama-kings-speech-title">
+                    Kings Speech
+                  </p>
+                  <div className="browse-drama-kings-speech-close-icon-wrapper">
+                    <img
+                      className="browse-drama-kings-speech-close-icon"
+                      src="/images/icons/close.png"
+                      onClick={() => setShowKingsSpeechInfo(false)}
+                    ></img>
+                  </div>
+                </div>
+                <p className="browse-drama-kings-speech-subtitle">
+                  King George VI tries to overcome his stammering problem with
+                  the help of speech therapist Lionel Logue and makes himself
+                  worthy enough to lead his country through World War II.
+                </p>
+                <button className="browse-header-play-button">Play</button>
+              </div>
+            </div>
+          ) : null}
         </div>
+
+        {/* **************** End Of The Drama Movies ********************** */}
+
+        {/* **************** Start Of The Thriller Movies ***************** */}
 
         <div className="browse-thriller">
           <p className="browse-thriller-title">Thriller</p>
